14af52595470901921fdb6178b7336abb2832b5a
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2021-12-04 13:30:10 +0300

    Initial Commit

---
+++ FacebookFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookFriends
+//
+//  Created by Rumeysa Yücel on 4.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
